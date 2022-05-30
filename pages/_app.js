7c3f3b61fccc8214a8c629b995e5e2d88568082f
Juraj Basanda <basanda.juraj@gmail.com>
2022-05-30 21:41:16 +0100

    add storyblok

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import { storyblokInit, apiPlugin } from '@storyblok/react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	storyblokInit({
+		accessToken: process.env.STORYBLOK_API_KEY,
+		use: [apiPlugin],
+	})
+
+	return <Component {...pageProps} />
 }
 
 export default MyApp
---
+++ utils/stotyblok-service.js
@@ -0,0 +1,1 @@
+import Storyblok from 'storyblok-js-client'
